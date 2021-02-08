45f16f186047c7721abd1c43917ae09657fe2ffe
Danny Tomkins <tomkinsdan@gmail.com>
2021-02-08 09:27:18 -0600

    deploying to heroku

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 17-NoSQL-Workout-Tracker
 # Unit 17 NoSQL Homework: Workout Tracker
+
+atlas username dtomkins
+password M21tDQ5PgRFdZQql
--- server.js
+++ server.js
@@ -11,7 +11,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI  ||
-    "mongodb://localhost/workout" )  ;
+    "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+    )  ;
 
 // routes
 app.use(require("./routes/htmlroutes"))
