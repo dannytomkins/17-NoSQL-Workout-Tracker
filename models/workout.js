dc4ab0b9c066c7d56497a4cd80a12f2ccf27a1e5
Danny Tomkins <tomkinsdan@gmail.com>
2021-02-13 18:32:47 -0600

    correct totalduration issue. add virtual component

--- models/workout.js
+++ models/workout.js
@@ -6,9 +6,26 @@ var workoutSchema = new Schema({
         type: Date,
         default: new Date().setDate(new Date().getDate())
     },
-    exercises: Array,
-    totalDuraton: Number
-})
+    exercises: Array 
+}, {
+    toJSON: {
+    virtuals: true 
+    }
+  })
+
+  
+workoutSchema
+.virtual('totalDuration')
+.get(function () {
+    var totalDuration = 0  
+    for (let i = 0; i < this.exercises.length; i++) {
+       
+        totalDuration = totalDuration + this.exercises[i].duration 
+    }
+
+  return  totalDuration;
+});
+
 
 var Workout = mongoose.model("Workout", workoutSchema)
 
